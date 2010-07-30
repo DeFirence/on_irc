9e9daca3abb9e55a708bf8d91732611df5a91900
DeFirence <defirence@defirence.za.net>
2010-07-30 14:32:39 +0200

    neatened and corrected user initialization

--- lib/on_irc/event.rb
+++ lib/on_irc/event.rb
@@ -5,7 +5,9 @@ class IRC
     def initialize(server, prefix, command, target, params)
       @server = server
       @sender = Sender.new(prefix)
-      @sender.user = server.user[@sender.nick] if server.users.include? @sender.nick
+      unless @sender.server? and server.users.include? @sender.nick
+        @sender.user = server.user[@sender.nick]
+      end
       @command = command
       @target = target
       @channel = target if target and target[0] == 35
