9d96a4a3102d868014bee77ed73a14a8f66747d2
DeFirence <defirence@defirence.za.net>
2012-04-29 12:55:55 +0200

    use existing instance_exec method if defined

--- lib/on_irc/event.rb
+++ lib/on_irc/event.rb
@@ -20,16 +20,18 @@ end
 
 
 module Kernel
- # Like instance_eval but allows parameters to be passed.
-  def instance_exec(*args, &block)
-    mname = "__instance_exec_#{Thread.current.object_id.abs}_#{object_id.abs}"
-    Object.class_eval{ define_method(mname, &block) }
-    begin
-      ret = send(mname, *args)
-    ensure
-      Object.class_eval{ undef_method(mname) } rescue nil
+  unless Kernel.respond_to? :instance_exec
+    # Like instance_eval but allows parameters to be passed.
+    def instance_exec(*args, &block)
+      mname = "__instance_exec_#{Thread.current.object_id.abs}_#{object_id.abs}"
+      Object.class_eval{ define_method(mname, &block) }
+      begin
+        ret = send(mname, *args)
+      ensure
+        Object.class_eval{ undef_method(mname) } rescue nil
+      end
+      ret
     end
-    ret
   end
 end
 
